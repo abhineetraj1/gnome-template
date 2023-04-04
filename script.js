4edb9ef76b974f0144419cc269ea6b18db444dc5
Abhineet Raj <82033840+abhineetraj1@users.noreply.github.com>
2023-04-04 07:24:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 ign=["Accessories-Text-Editor.png","computer.png","recycle-bin.png","terminal.png","network.png","shark.png"];
-var a = [document.createElement("div"), document.createElement("img"),document.createElement("nav"),[]];
+var a = [document.createElement("footer"), document.createElement("center"),document.createElement("nav"),[]];
 for (var i = ign.length - 1; i >= 0; i--) {
 	var t = document.createElement("img");
 	t.src="png/"+ign[i];
 	t.className="nv-ig";
 	a[3].push(t);
 }
-a[1].src="png/bg.jpg";
-a[0].id="content";
-a[1].id="bg-img";
 a[2].id="nv";
 a[0].appendChild(a[1]);
-a[0].appendChild(a[2])
+a[1].appendChild(a[2]);
 for (var i = a[3].length - 1; i >= 0; i--) {
 	a[2].appendChild(a[3][i]);
 }
+document.body.appendChild(a[0]);
